7eb2e7dad56dcfac62fe0023cc0515875fe953f9
SBEROL <other.autre@outlook.fr>
2019-07-16 14:46:40 +0200

    Import Module S.B

---
+++ client.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3.6
+from __future__ import print_function
+import os
+import sys
+import socket
+import select
---
+++ server.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3.6
+from __future__ import print_function
+import os
+import sys
+import socket
