53f6dd7b30075f19c28ca0db097e73a290eb3de5
SBEROL <other.autre@outlook.fr>
2019-07-20 13:58:02 +0200

    bon début

--- client.py
+++ client.py
@@ -1,6 +1,77 @@
 #!/usr/bin/python3.6
+#shebang
+
+#Nom            : Serveur concurrent
+#Description    : Vous allez écrire un client et un serveur en python 3 sous Linux. Ce sera un simili-telnet.
+#Ex)            : server.py 1234
+#Param 1        : Script en lui même
+#Param 2        : IP Server
+#Param 3        : Port Server
+#Version        : 0.1
+#Auteur         : BEROL Saba & HUG Nicolas & RIOS Bastien
+#Changelog      : Aucun
+
+# IMPORT PART
+# ----------------
+
 from __future__ import print_function
 import os
 import sys
 import socket
-import select
+
+# FUNCTION PART
+# ----------------
+
+# S'il le manque ne serait-ce qu'un seule des arguements, sort du script.
+if len(sys.argv)<4:
+	print("Usage :",sys.argv[0],"IP_server port_server")
+	sys.exit(-1)
+
+if sys.argv[3]!="stdin": # la source de donnees est un fichier
+	# on ouvre le fichier
+	fd=os.open(sys.argv[3],os.O_RDONLY)
+else:	# la source de donnees est le clavier
+        fd=sys.stdin.fileno()
+
+connectes=[fd] # on met le descripteur de l'entree de donnees dans les descripteurs a surveiller
+
+end_data=0 	# si end_data=1, c'est qu'on n'a plus de donnees a envoyer
+		# et qu'on a fait un shutdown sur la socket
+
+# creation de la socket TCP/IPv4
+sockfd=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+# connexion au serveur dont l'adresse et le port sont passes en arguments
+sockfd.connect((sys.argv[1],int(sys.argv[2])))
+# on ajoute le descripteur de socket aux descripteurs a surveiller
+connectes.append(sockfd)
+
+while True:
+	# on se met en attente d'un evenement en lecture
+	# sur la socket ou l'entree de donnees
+	# on recupere dans a_lire le(s) descripteur(s) prets a lire
+	a_lire,[],[]=select.select(connectes,[],[])
+
+	# on parcourt la liste des descripteurs sur lesquels un evenement s'est produit
+	for desc in a_lire:
+		if desc==sockfd: # evenement sur la socket
+			recu=sockfd.recv(1024) # on lit sur la socket
+			if len(recu)!=0: # on a recu quelque chose
+				# affichage de ce qui a ete recu sur la socket
+				recu=str(recu,'latin')
+				print(recu,end='') # sans retour chariot
+			else:	# si on n'a rien recu
+				if end_data==0: # et qu'on n'a pas fait shutdown precedemment
+					# alors c'est que le serveur a mis fin a la connexion
+					print("Deconnexion du serveur")
+					sys.exit(-1)
+				else:	sys.exit(0) # sinon, fin normale
+
+		if desc==fd: # evenement sur l'entree de donnees
+			# on lit sur l'entree de donnees
+			lu=os.read(fd,4096)
+			if len(lu)==0: # si on n'a rien lu (EOF)
+				sockfd.shutdown(socket.SHUT_WR) # on ferme la socket en ecriture
+				end_data=1 # on marque la fin des donnees
+				connectes=[sockfd] # on ne surveille plus que la socket
+			else: # sinon on envoie dans la socket ce qu'on a lu
+                       		sockfd.send(lu)
--- server.py
+++ server.py
@@ -1,5 +1,165 @@
 #!/usr/bin/python3.6
+#shebang
+
+#Nom            : Serveur concurrent
+#Description    : Vous allez écrire un client et un serveur en python 3 sous Linux. Ce sera un simili-telnet.
+#Ex)            : server.py 1234
+#Param 1        : Script en lui même
+#Param 2        : port d'écoute
+#Version        : 0.1
+#Auteur         : BEROL Saba & HUG Nicolas & RIOS Bastien
+#Changelog      : Aucun
+
+# IMPORT PART
+# ----------------
+
 from __future__ import print_function
 import os
 import sys
 import socket
+
+# FUNCTION PART
+# ----------------
+
+def serveur_fils():
+	sockfd.close()
+	while True:
+		recu=connfd.recv(1024)
+		if len(recu)==0:
+			print("Deconnexion de",client)
+			connfd.close()
+			sys.exit(0)
+		print(recu)
+		connfd.send(recu)
+
+
+# Création du père
+def server_pere():
+
+	# S'il le manque un argument ("port" ici), sort du script.
+	if len(sys.argv)!=3:
+		print("Usage : ",sys.argv[0],"name_file_user","n°_port")
+		sys.exit(-1)
+	try:
+		fd=os.open(sys.argv[1],os.O_RDONLY)
+	except OSError:
+		print("erreur d'ouverture de",sys.argv[1])
+	sys.exit(-1)
+
+	
+
+	# Initialisation du port d'écoute
+	sockfd=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	sockfd.bind(('',int(sys.argv[1])))
+	# Autorise jusqu'a 10 connexions
+	sockfd.listen(10)
+
+	# Initialisation de la connexion
+	while True:
+		print("Attente d'un client")
+		connfd,client=sockfd.accept()
+		print("Connection de",client)
+		child=os.fork()
+		if child==0:
+			serveur_fils()
+		else:
+			connfd.close()
+
+
+# INSTRUCTION PART
+# ----------------
+
+    
+    # Il faut créer une instruction qui lit dans le fichier habilitation les users et le mdp
+    #data = open("essai.txt", "r").read()
+    #if nom_du_reseau in data :
+    #    print("ok")
+
+    message = "WHO : "
+    conn.send(message.encode())
+    dat = conn.recv(1024).decode()
+    dat = str(dat)
+    if not dat:
+        conn.close()
+    if valid_id_client == dat:
+        message = ' -> Enter the password:'
+        conn.send(message.encode())
+        dat = conn.recv(1024).decode()
+        if not dat:
+            conn.close()
+        if valid_passwd_client != dat:
+            message = ' -> Authentication failed. Because of bad password BYE'
+            conn.send(message.encode())
+            conn.close()
+        else:
+            message = 'Leggo. U connected bish'
+            conn.send(message.encode())
+    else:
+        message = ' -> Bad id ! BYE'
+        conn.send(message.encode())
+        conn.close()
+
+    while True:
+        data = conn.recv(1024).decode()
+        if not data:
+            # if data is not received break
+            break
+        if data == "rls":
+            files = []
+            repo = []
+            stop="stop"
+            print("User asked my repo")
+            for i in os.listdir(os.getcwd()):
+                if os.path.isfile(i):
+                    files.append(i)
+                elif os.path.isdir(i):
+                    repo.append(i)
+            for x in files:
+                print('Fil -', x)
+                conn.send(x.encode())
+            conn.send(stop.encode())
+            for y in repo:
+                print('Repo -', y)
+                conn.send(y.encode())
+            conn.send(stop.encode())
+
+        if data == "rpwd":
+            print("User asked whereiam")
+            where = os.getcwd().encode()
+            conn.send(where)
+
+        if data == "rcd":
+            files = []
+            repo = []
+            stop="stop"
+            print("User asked me to go somewhere")
+            for i in os.listdir(os.getcwd()):
+                if os.path.isfile(i):
+                    files.append(i)
+                elif os.path.isdir(i):
+                    repo.append(i)
+            for x in files:
+                print('Fil -', x)
+                conn.send(x.encode())
+            conn.send(stop.encode())
+            for y in repo:
+                print('Repo -', y)
+                conn.send(y.encode())
+            conn.send(stop.encode())
+
+            where = conn.recv(1024).decode()
+            print("ask to go there : " + where)
+            cwd = os.getcwd()
+            try: 
+                os.chdir(where) 
+                print("Now u r there:" + os.getcwd()) 
+            except: 
+                os.chdir(cwd)
+                print("Not a repo ", sys.exc_info())    
+            conn.send(os.getcwd().encode())
+
+    conn.close()  # close the connection
+
+
+if __name__ == '__main__':
+    Server()
